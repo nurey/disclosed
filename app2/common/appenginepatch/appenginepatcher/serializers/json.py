a3eaabbe58973c472ae0b18e248f01dc10793a3d
ilia <ilia@9ec27b70-f34a-0410-b428-dc3e63c20f86>
2009-03-06 20:51:18 +0000

    Merging app-engine-patch 1.0
    git-svn-id: http://svn.nurey.com/goat/trunk@145 9ec27b70-f34a-0410-b428-dc3e63c20f86

--- app2/common/appenginepatch/appenginepatcher/serializers/json.py
+++
@@ -1,4 +0,0 @@
-from django.core.serializers import json
-from python import Deserializer
-json.PythonDeserializer = Deserializer
-from django.core.serializers.json import *
--- app2/common/appenginepatch/appenginepatcher/serializers/yaml.py
+++
@@ -1,4 +0,0 @@
-from django.core.serializers import pyyaml
-from python import Deserializer
-pyyaml.PythonDeserializer = Deserializer
-from django.core.serializers.pyyaml import *
